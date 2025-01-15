46a10db5a32472ad2df8d70de8caa77e6cfbda1b
tfenne <tfenne@tfenne.com>
2025-01-15 06:25:20 -0700

    Added docs and basic tests.

--- prymer/__init__.py
+++ prymer/__init__.py
@@ -3,5 +3,6 @@ from prymer.model import Oligo
 from prymer.model import PrimerPair
 from prymer.model import Span
 from prymer.model import Strand
+from prymer.thermo import Thermo
 
-__all__ = ["Strand", "Span", "Oligo", "PrimerPair", "MinOptMax"]
+__all__ = ["Strand", "Span", "Oligo", "PrimerPair", "MinOptMax", "Thermo"]
--- prymer/thermo.py
+++ prymer/thermo.py
@@ -9,23 +9,38 @@ from primer3.thermoanalysis import _ThermoAnalysis
 @dataclass(frozen=True, kw_only=True)
 class Thermo:
     """
-    Class for performing thermodynamic calculations.  Largely wraps the `_ThermoAnalysis`
-    class from `primer3-py` to provide a more ergonomic interface and reset default thermodynamic
-    parameters to match those used in primer3.
+    Class for performing thermodynamic calculations.  Available calculations include:
 
-    mv_conc_mm: concentration of monovalent cations in mM
-    dv_conc_mm: concentration of divalent cations in mM
-    dntp_conc_mm: concentration of deoxynycleotide triphosphate in mM
-    dna_conc_nm: concentration of DNA strands in nM
-    dmso_conc_pct: Concentration of DMSO (%)
-    dmso_correction_factor: DMSO correction factor
-    formamide_conc_mol: Concentration of formamide (mol/l)
-    annealing_temp_c: Actual annealing temperature of the PCR reaction in Celsius
-    temp_c: Simulation temperature for structure prediction in Celsius
-    max_nn_length: Maximum length in bases for nearest-neighbor calcs
-    max_loop: maximum size of loops in bases when predicting structures
-    tm_method: Tm calculation method
-    salt_corrections_method: Salt correction method
+      1. melting temperature (Tm) for short and long sequences
+      2. hairpin / secondary structure Tm for single sequences
+      3. homodimer Tm - the Tm of a duplex formed from two copies of the same sequence
+      4. heterodimer Tm - the Tm of a duplex formed from two different sequences
+      5. 3' anchored heterodimer Tm - the heterodimer Tm when annealing of the 3' end is prioritized
+
+    The `tm` method can be used for sequences of any length.  For sequences up to `max_nn_length`
+    (default value 60 bases) the nearest neighborhood Tm calculation is used.  For sequences above
+    the `max_nn_length` a more generic long sequence Tm calculation is used.
+
+    Many attributes affect the calculation of Tms and these are set to the default values used by
+    primer3 and, where applicable, ntthal.
+
+    Class level constants are provided for Tm calculation method names and salt correction method
+    names, and it is strongly suggested to use these.
+
+    Attributes:
+        mv_conc_mm: concentration of monovalent cations in mM
+        dv_conc_mm: concentration of divalent cations in mM
+        dntp_conc_mm: concentration of deoxynycleotide triphosphate in mM
+        dna_conc_nm: concentration of DNA strands in nM
+        dmso_conc_pct: Concentration of DMSO (%)
+        dmso_correction_factor: DMSO correction factor
+        formamide_conc_mol: Concentration of formamide (mol/l)
+        annealing_temp_c: Actual annealing temperature of the PCR reaction in Celsius
+        temp_c: Simulation temperature for structure prediction in Celsius
+        max_nn_length: Maximum length in bases for nearest-neighbor calcs
+        max_loop: maximum size of loops in bases when predicting structures
+        tm_method: Tm calculation method
+        salt_corrections_method: Salt correction method
     """
 
     TM_METHOD_BRESLAUER: ClassVar[str] = "breslauer"
@@ -52,6 +67,7 @@ class Thermo:
     _thermo: _ThermoAnalysis = field(init=False, default_factory=lambda: _ThermoAnalysis())
 
     def __post_init__(self) -> None:
+        """Transfers the values to the underlying implementation class."""
         self._thermo.set_thermo_args(
             mv_conc=self.mv_conc_mm,
             dv_conc=self.dv_conc_mm,
@@ -68,21 +84,111 @@ class Thermo:
             salt_corrections_method=self.salt_corrections_method,
         )
 
+    # @staticmethod
+    # def from_primer_params(p: PrimerAndAmpliconParameters) -> "Thermo":
+    #     return Thermo(
+    #         mv_conc_mm=p.,
+    #         dv_conc_mm=p.,
+    #         dntp_conc_mm=p.,
+    #         dna_conc_nm=p.,
+    #         dmso_conc_pct=p.,
+    #         dmso_correction_factor=p.,
+    #         formamide_conc_mol=p.,
+    #         annealing_temp_c=p.,
+    #         temp_c=p.,
+    #         max_nn_length=p.,
+    #         max_loop=p.,
+    #         tm_method=p.,
+    #         salt_corrections_method=p.,
+    #     )
+    #
+    # @staticmethod
+    # def from_probe_params(p: ProbeParameters) -> "Thermo":
+    #     return Thermo(
+    #         mv_conc_mm=p.,
+    #         dv_conc_mm=p.,
+    #         dntp_conc_mm=p.,
+    #         dna_conc_nm=p.,
+    #         dmso_conc_pct=p.,
+    #         dmso_correction_factor=p.,
+    #         formamide_conc_mol=p.,
+    #         annealing_temp_c=p.,
+    #         temp_c=p.,
+    #         max_nn_length=p.,
+    #         max_loop=p.,
+    #         tm_method=p.,
+    #         salt_corrections_method=p.,
+    #     )
+
+
     def tm(self, bases: str) -> float:
+        """
+        Calculates the melting temperature of a sequence of bases.  The calculation used differs
+        for short sequences (len < `max_nn_length`) and long sequences.
+
+        Arguments:
+            bases: a sequence of unambiguous DNA bases (i.e. A/C/G/T)
+
+        Returns:
+            the melting temperature of the sequence, in degrees Celsius
+        """
         return float(self._thermo.calc_tm(bases))
 
     def hairpin_tm(self, bases: str) -> float:
+        """
+        Calculates the melting temperature of the most likely secondary structure or hairpin of a
+        single oligo.
+
+        Arguments:
+            bases: a sequence of unambiguous DNA bases (i.e. A/C/G/T)
+
+        Returns:
+            the melting temperature of the most likely (lowest energy) secondary structure
+        """
         result: ThermoResult = self._thermo.calc_hairpin(bases)
         return float(result.tm)
 
     def homodimer_tm(self, bases: str) -> float:
+        """
+        Calculates the melting temperature of the most likely annealing of a single oligo to a
+        second copy of the same oligo (i.e. a duplex of two of the same oligo).
+
+        Arguments:
+            bases: a sequence of unambiguous DNA bases (i.e. A/C/G/T)
+
+        Returns:
+            the melting temperature of the most likely (lowest energy) duplex
+        """
         result: ThermoResult = self._thermo.calc_homodimer(bases)
         return float(result.tm)
 
     def heterodimer_tm(self, bases1: str, bases2: str) -> float:
+        """
+        Calculates the melting temperature of the most likely annealing of two oligos.  The two
+        oligos do *not* need to be the same length.
+
+        Arguments:
+            bases1: a sequence of unambiguous DNA bases (i.e. A/C/G/T) for the first oligo
+            bases2: a sequence of unambiguous DNA bases (i.e. A/C/G/T) for the second oligo
+
+        Returns:
+            the melting temperature of the most likely (lowest energy) heterodimer
+        """
+
         result: ThermoResult = self._thermo.calc_heterodimer(bases1, bases2)
         return float(result.tm)
 
-    def heterodimer_3p_tm(self, bases1: str, bases2: str) -> float:
+    def heterodimer_3p_anchored_tm(self, bases1: str, bases2: str) -> float:
+        """
+        Calculates the melting temperature of the most likely annealing of two oligos where the
+        annealing is constrained to optimize annealing of the 3' end of the first sequence.
+
+        Arguments:
+            bases1: a sequence of unambiguous DNA bases (i.e. A/C/G/T) for the first oligo
+            bases2: a sequence of unambiguous DNA bases (i.e. A/C/G/T) for the second oligo
+
+        Returns:
+            the melting temperature of the most likely (lowest energy) 3' anchored heterodimer
+        """
         result: ThermoResult = self._thermo.calc_end_stability(bases1, bases2)
         return float(result.tm)
